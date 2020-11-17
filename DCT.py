25af54002e6283d81ca39ff491f7cd672410715b
PabloAcedo <pablo.acedo01@estudiant.upf.edu>
2020-11-17 15:24:09 +0100

    three

--- DCT.py
+++ DCT.py
@@ -1,14 +1,35 @@
-# import matplotlib.pyplot as plt
 import scipy
+import scipy.fftpack
 import sys
+import imageio
+import numpy as np
 
 
-def dct(data):
-    return scipy.fftpack.dct(data)
+# 2d signal dct using scipy
+def dct2D(data):
+    return scipy.fftpack.dct(scipy.fftpack.dct(data, axis=1, norm='ortho'), axis=1, norm='ortho')
 
 
-def idct(data):
-    return scipy.fftpack.idct(data)
+# 2d signal idct using scipy
+def idct2D(data):
+    return scipy.fftpack.idct(scipy.fftpack.idct(data, axis=1, norm='ortho'), axis=1, norm='ortho')
 
 
-print(dct(sys.argv[1]))
+# reading image with imageio
+signal = imageio.imread('Lenna.png')
+# image size
+size = signal.shape
+
+# computing the dct of the image (of each one of the rgb channels)
+X = np.zeros(size)
+for k in range(3):
+    X[:, :, k] = dct2D(signal[:, :, k])
+
+# reconstructing the image from the compressed dct
+y = np.zeros(size)
+for k in range(3):
+    y[:, :, k] = idct2D(X[:, :, k])
+
+# write reconstructed image to a file
+imageio.imwrite('restored_Lenna.png',
+                y)  # there will be no change in the image quality since we are not applying compression
--- rgb_yuv.py
+++ rgb_yuv.py
@@ -20,6 +20,7 @@ def yuv2rgb(Y, U, V):
     return int(r), int(g), int(b)
 
 
+# function to check if in range
 def in_range(p1, p2, p3, min, max):
     if min <= p1 <= max and min <= p2 <= max and min <= p3 <= max:
         return 1
@@ -27,20 +28,21 @@ def in_range(p1, p2, p3, min, max):
     return 0
 
 
+# check that the number of arguments are correct
 if len(sys.argv) == 5:
     # input parameters
     p1 = int(sys.argv[1])
     p2 = int(sys.argv[2])
     p3 = int(sys.argv[3])
     option = sys.argv[4]
-
+    # if rgb2yuv
     if option == 'rgb2yuv':
         if in_range(p1, p2, p3, 0, 255) == 1:
             YUV = rgb2yuv(p1, p2, p3)
             print('\nRGB:', p1, p2, p3, '--> YUV:', YUV[0], YUV[1], YUV[2], '\n')
         else:
             print('\nERROR: Enter only RGB values, 3 values from 0 to 255.\n')
-
+    # if yuv2rgb
     elif option == 'yuv2rgb':
         RGB = yuv2rgb(p1, p2, p3)
         print('\nYUV:', p1, p2, p3, '--> RGB:', RGB[0], RGB[1], RGB[2], '\n')
